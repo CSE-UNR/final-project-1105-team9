6fb2a7e829d7a73531a85705e171847a83b97fe4
Yuliza Farias <yfarias@unr.edu>
2024-12-05 17:35:06 +0000

    final project

---
+++ madlibs.c
@@ -0,0 +1,88 @@
+//Final Project
+
+
+
+#include <stdio.h> 
+
+#define MAX_LENGTH 100
+#define MAX_WORDS 50
+
+void wordTypes(FILE *file , char *wordTypes);
+void getInput(char *wordTypes , char input[MAX_WORDS][MAX_LENGTH];
+void create(char *madlib , char *wordTypes , char input[MAX_WORDS][MAX_LENGTH];
+int main();
+
+
+void getWord(FILE *file , char *wordTypes){
+	int i = 0;
+	char ch;
+
+
+	while(fscanf)file , "%c" , &ch) == 1){
+		if(ch == '\n'){
+			break;
+		}else if(ch != ','){
+			wordTypes[i++] = ch;
+		}
+	}
+		wordTypes[i] = '\0';
+}
+
+void getInput(char *wordTypes , char input[MAX_WORDS][MAX_LENGTH]{
+	int i = 0;
+	while(wordTypes[i] != '\0'){
+		if(wordTypes[i] == 'N'{
+			printf("Enter a noun: ");
+		}else if(wordTypes[i] == 'A'){
+			printf("Enter an adjective: ");
+		}else if(wordTypes[i] == 'V');{
+			printf("Enter a verb: ");
+		}
+		scanf("%s" , input[i]);
+		i++;
+	}
+}
+
+void create(char *madlib , char *wordTypes , char input[MAX_WORDS][MAX_LENGTHS]){
+	int i = 0;
+	int j = 0;
+	char filledMadlib[MAX_LENGTH * 2] = ' ';
+	int filledIndex = 0;
+	
+	
+	while(madlib[i] != '\0'){
+		if(madlib[i] == 'A' || madlib[i] == 'N' || madlib[i] == 'V'{
+		while(wordTypes[j] != '\0' && wordTypes[j] != madlib[i]){
+		j++
+	}
+	int k = 0 
+	while(input[j][k] != '\0';
+		filledMadlib[filledIndex++] = input[j][k++];
+		}
+	}else{
+		filledMadlib[filledIndex++] = madlib[i];
+	}
+		i++;
+}
+
+	filledMadlib[filledIndex] = '\0';
+	//printf("   ?    ");
+
+
+
+
+int main(){
+
+
+	FILE *file = fopen("wordtypes.txt" , "r");
+		if(file == NULL){
+			printf("NOPE\n");
+		return;
+		}
+		
+	getWord(file , wordTypes);
+	fclose(file);
+
+	return 0;
+}
+
