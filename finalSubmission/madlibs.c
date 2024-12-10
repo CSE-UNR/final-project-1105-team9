f2f6fc3c0e0052600b00451261c039e203adb3d9
Hawkeye Bufkin Plank <hawkeyeplank@gmail.com>
2024-12-10 14:41:42 -0800

    "hawkeye working on code"

--- finalSubmission/madlibs.c
+++ finalSubmission/madlibs.c
@@ -9,6 +9,33 @@ void getInput(char *wordTypes);
 //void create(char *madlib , char *wordTypes , char input[MAX_WORDS][MAX_LENGTH]);
 
 int main(){
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	FILE *fp = fopen("madlib1.txt" , "r");
 	if(fp == NULL){
@@ -47,49 +74,6 @@ int main(){
 
 
 //////////////////////////////////////////////////////////////////////////////////////
-int main(){
-    FILE* readPTR = fopen(INFILE, "r");
-
-    if(readPTR == NULL){printf("File didn't open\n"); return 0;}else{printf("opened!\n");}
-        int lineCounter = 0;        
-        char lineStore[NLINES][MAX_LENGTH];
-        for(int lineIndex = 0; lineIndex < NLINES; ++lineIndex){
-                fgets(lineStore[lineIndex], 100, readPTR);
-                if(*lineStore[lineIndex] == '\0'){break;}
-                    lineCounter++;
-        }
-    fclose(readPTR);
-    char types[numTypeLines(lineCounter, MAX_LENGTH, lineStore)];
-    int stepthrough = 0;
-        for(int i = 0;i <lineCounter; i++){
-            
-            if(lineLength(lineStore[i]) == 2){
-                if(lineStore[i][0] == 'A' | lineStore[i][0] == 'N'| lineStore[i][0] == 'V'){  
-                    types[stepthrough] = lineStore[i][0];
-                    stepthrough++;
-                }
-            }
-        }
-    for(int i = 0; i<stepthrough;i++){
-        printf("%c", types[i]);
-    }
-return 0;
-}
-
-
-int lineLength(char inword[]){
-    int counter = 0;
-        for(counter;inword[counter] != '\0';counter++){;}
-    return(counter);
-}
-
-int numTypeLines(int numReadLines, int maxLen, char inmat[][maxLen]){
-    int counter = 0;
-        for(int i = 0;i < numReadLines; i++){
-            if(lineLength(inmat[i]) == 2){counter++;}
-        }
-    return(counter);
-}
 
 
     
