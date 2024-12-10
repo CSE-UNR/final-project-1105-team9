447bef5068f7b0d9b3bcbcaf6985955c7aacdb3e
1105-Farias-Yuliza <yfarias@unr.edu>
2024-12-09 22:39:42 -0800

    Update madlibs.c

--- finalSubmission/madlibs.c
+++ finalSubmission/madlibs.c
@@ -1,23 +1,37 @@
 #define INFILE "madlib1.txt"
 #define NLINES 60
 #define MAX_LENGTH 100
-#define MAX_WORDS 50
 int lineLength(char inword[]);
 int numTypeLines(int numReadLines, int maxLen, char inmat[][maxLen]);
 void gettwodarray(char types[]);
 void getTypes(FILE *file , char *wordTypes);
 void getInput(char *wordTypes , char input[MAX_WORDS][MAX_LENGTH]);
-void create(char *madlib , char *wordTypes , char input[MAX_WORDS][MAX_LENGTH]);
+//void create(char *madlib , char *wordTypes , char input[MAX_WORDS][MAX_LENGTH]);
 
 int main(){
+FILE *fp = fopen("madlib1.txt" , "r");
+	if(fp == NULL){
+		printf("NOPE\n");
+		return 0;
+	}
 
-
-
-
-
-
-return 0;
+	
+	char ch;
+	
+	while(fscanf(fp , "%c" , &ch) == 1){
+		if(ch == '\n'){
+			break;
+		}
+		
+	printf("%c" , ch);
 }
+		
+	//void getTypes(FILE *fp , char *wordTypesArray);
+	fclose(fp);
+
+	return 0;
+}
+
 
 
 
